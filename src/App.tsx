a4074cd08d4d93551c1643dc39d1a715a90ec17e
Andy Wang <andy_wang@comcast.net>
2020-10-09 12:10:18 -0700

    feat(utils): add board generate function

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import './App.css';
+import styled, { StyledComponent } from 'styled-components';
 
-function App() {
-  return <div className="App"></div>;
+const StyledApp = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+function App(): StyledComponent<'div', any, {}, never> {
+  return <StyledApp></StyledApp>;
 }
 
 export default App;
---
+++ src/utils/boardGenerator.ts
@@ -0,0 +1,20 @@
+interface cellTemplate {
+  piece: '-' | 'red' | 'black';
+  isSelected: boolean;
+  isAvailableMove: boolean;
+}
+const cellTemplate = { piece: '-', isSelected: false, isAvailableMove: false };
+
+export const generateBoardState = (dimensions: number) => {
+  //create matrix of equal number of rows and columns
+
+  const board = [...Array(dimensions)].map((el) =>
+    [...Array(dimensions)].map((cell) => {
+      return { ...cellTemplate };
+    })
+  );
+
+  return board;
+};
+
+console.log(generateBoardState(4));
