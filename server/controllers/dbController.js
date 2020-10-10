9517d19a2d113bf37ee54c4020fc27807a1ef342
Andy Wang <andy_wang@comcast.net>
2020-10-09 20:53:54 -0700

    feat(server):finish get gamestate controller

--- server/controllers/dbController.js
+++ server/controllers/dbController.js
@@ -5,13 +5,22 @@ const dbController = {};
 
 dbController.addGameState = (req, res, next) => {
   const gameState = req.body.gameState;
-  const stringifiedGameState = JSON.stringify(gameState);
 
-  GameGroup.create({ gameState: stringifiedGameState }, (err, data) => {
+  GameGroup.create({ gameState }, (err, data) => {
     if (err) return next(err);
-
     //id of new DB entry will be sent back to user
     res.locals.id = data._id;
+    return next();
+  });
+};
+
+dbController.getGameState = (req, res, next) => {
+  const id = req.body.id;
+
+  GameGroup.findById(id, (err, data) => {
+    if (err) return next(err);
+
+    res.locals.gameState = data.gameState;
     return next();
   });
 };
--- server/routes/api.js
+++ server/routes/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dbController = require('../controllers/dbController');
 const router = express.Router();
 
-router.get('/', (req, res) => res.json('placeholder'));
+router.get('/', dbController.getGameState, (req, res) => res.json(res.locals.gameState));
 router.post('/', dbController.addGameState, (req, res) => res.json(res.locals.id));
 
 module.exports = router;
